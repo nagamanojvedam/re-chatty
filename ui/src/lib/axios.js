e62f4d2d8fdde50186e6f19bdcc8265c2097a650
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2025-07-16 19:51:51 +0530

    fixed axios instance url

--- ui/src/lib/axios.js
+++ ui/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   import.meta.env.VITE_ENV === "development"
     ? "http://localhost:5000/api/v1"
-    : "/";
+    : "/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
